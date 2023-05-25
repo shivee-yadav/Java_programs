4c6dfb20c34a536ea5732234ae224a0f3b0cf4da
shivee yadav <shiveeyadav089@gmail.com>
2023-05-25 23:35:34 +0530

    new commit

---
+++ InheritanceC.java
@@ -0,0 +1,10 @@
+
+public class InheritanceC {
+	public InheritanceC() {
+		System.out.println("c");
+	}
+	public void abc() {
+		System.out.println();
+	}
+
+}
---
+++ Interf1.java
@@ -0,0 +1,17 @@
+
+public interface Interf1 {
+	void mymethod1();//by default public and abstract
+	public abstract void mymethod2();//writing public and abstract is optional
+
+}
+
+class Test20 implements Interf1{
+	
+	public void mymethod1() {//here writing public is compulsory 
+		
+	}
+	public void mymethod2() {
+		
+	}
+	
+}
